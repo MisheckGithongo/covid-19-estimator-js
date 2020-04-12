7c36eea03b69bb1080a6c96158cb4409261b4e8c
Misheck Githongo <misheckgithongo@gmail.com>
2020-04-12 13:31:28 +0300

    added a sign checker function

--- src/estimator.js
+++ src/estimator.js
@@ -16,13 +16,22 @@ const covid19ImpactEstimator = (data) => {
     factor = Math.floor((days) / 3);
     sets = 2 ** factor;
   }
+  const signChecker = (num) => {
+    if (num > 0) {
+      return Math.floor(num);
+    }
+    if (num < 0) {
+      return Math.ceil(num);
+    }
+    return null;
+  };
   return {
     data: input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
       infectionsByRequestedTime: input.reportedCases * 10 * sets,
       severeCasesByRequestedTime: Math.floor(0.15 * (input.reportedCases * 10 * sets)),
-      hospitalBedsByRequestedTime: Math.ceil((0.35 * input.totalHospitalBeds)
+      hospitalBedsByRequestedTime: signChecker((0.35 * input.totalHospitalBeds)
     - Math.floor(0.15 * (input.reportedCases * 10 * sets))),
       casesForICUByRequestedTime: Math.floor(0.05 * (input.reportedCases * 10 * sets)),
       casesForVentilatorsByRequestedTime: Math.floor(0.02 * (input.reportedCases * 10 * sets)),
@@ -34,7 +43,7 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: input.reportedCases * 50,
       infectionsByRequestedTime: input.reportedCases * 50 * sets,
       severeCasesByRequestedTime: 0.15 * (input.reportedCases * 50 * sets),
-      hospitalBedsByRequestedTime: Math.ceil((0.35 * input.totalHospitalBeds)
+      hospitalBedsByRequestedTime: signChecker((0.35 * input.totalHospitalBeds)
     - Math.floor(0.15 * (input.reportedCases * 50 * sets))),
       casesForICUByRequestedTime: Math.floor(0.05 * (input.reportedCases * 50 * sets)),
       casesForVentilatorsByRequestedTime: Math.floor(0.02 * (input.reportedCases * 50 * sets)),
